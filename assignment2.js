971a14ca5e81ab4b98e52f0ec5721ed7b8fbae0d
razaalisaylani <raza.ali.saylani@gmail.com>
2023-04-11 22:53:36 +0500

    Question # 05

--- assignment2.js
+++ assignment2.js
@@ -62,3 +62,14 @@ function addListItem(myText) {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(myText));
   }
+
+  /*
+  5. Write a function that changes the background color of an HTML element.
+The function should take two arguments: the first argument is a reference
+to the HTML element, and the second argument is a string representing
+the new background color
+  */
+
+function changeBackground(eId,color) {
+    document.getElementById(eId).style.background = color;
+ }
